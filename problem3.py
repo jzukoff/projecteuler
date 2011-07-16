dc7b2e979f9e2ff4fb8815bf9c862ca0ef939d5a
Josh Zukoff <josh.zukoff@gmail.com>
2011-07-15 23:39:00 -0400

    up to problem 3

---
+++ problem3.py
@@ -0,0 +1,27 @@
+import random
+import math
+# Josh Zukoff
+
+def is_prime(n, iterations=1):
+    for i in range(iterations):
+        randomint = random.randint(0,n-1)
+        if pow(randomint, n-1, n) != 1:
+            return False
+
+        
+
+    return True
+
+def main ():
+    currNum = 600851475143
+    lastPrime = 0
+    i = 2
+    while i <= currNum:
+        if currNum % i == 0 and is_prime(i):
+            while currNum % i == 0:
+                currNum = currNum / i
+                lastPrime = i
+        i += 1   
+    print lastPrime
+
+main()
